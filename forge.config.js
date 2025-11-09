8c002145e40f19225c6dab1977e2c5062010545a
lixlim <s2alua.fu@gmail.com>
2025-11-09 17:41:00 +0900

    ver1.13.0 : 실행아이콘 변경

--- forge.config.js
+++ forge.config.js
@@ -1,8 +1,17 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require('path');
+const isMac = process.platform === 'darwin';
+const isWin = process.platform === 'win32';
 
 module.exports = {
   packagerConfig: {
+    icon: path.resolve(
+      __dirname,
+      isMac ? 'static/icons/icon.icns'
+           : isWin ? 'static/icons/icon.ico'
+                   : 'static/icons/png/icon_512.png'
+    ),
     asar: {
       unpackDir: 'liquibase'
     },
@@ -10,12 +19,12 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {name: 'liquibase-gui-tool',},
+      config: { name: 'liquibase-gui-tool', icon: path.resolve(__dirname, 'static/icons/icon.ico') },
     },
     {
       name: '@electron-forge/maker-dmg',
       platforms: ['darwin'],
-      config: {}
+      config: { icon: path.resolve(__dirname, 'static/icons/icon.icns') }
     },
     {
       name: '@electron-forge/maker-deb',
